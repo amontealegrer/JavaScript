524a1e3aedda5a2693ea135814b7befa92e2cde6
Alexander Montealegre Ramirez <amontealegrer@gmail.com>
2025-07-18 16:02:02 -0500

    Actualización Clase 07

---
+++ ejercicios/Clase06/layout.js
@@ -0,0 +1,5 @@
+function toggleLayout() {
+    const container = document.getElementById("container");
+    container.classList.toggle('flex-layout');
+    container.classList.toggle('grid-layout');
+}
